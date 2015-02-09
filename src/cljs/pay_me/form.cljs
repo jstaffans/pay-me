d1d1a7a08ce0cb261f0d123e748f369a1cd4cb56
jstaffans <johannes.staffans@gmail.com>
2015-02-09 22:19:21 +0100

    Use core.async instead of plain callbacks

--- src/cljs/pay_me/form.cljs
+++ src/cljs/pay_me/form.cljs
@@ -1,19 +1,25 @@
 (ns pay-me.form
   (:require [domina :as dom]
             [domina.events :as ev]
-            [ajax.core :refer [POST]]))
+            [ajax.core :refer [POST]]
+            [cljs.core.async :refer [>! chan <!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn obfuscate [number]
   "Takes a credit card number and garbles a portion of it."
   (clojure.string/replace number #"\B." "*"))
 
+
 (defn get-token-and-submit! []
-  (POST "/verification" {:handler (fn [response]
-                                    (let [number-field (dom/by-id "number")]
-                                      (dom/append! (dom/by-id "pay-form") "<input id='token' type='hidden' name='token'/>")
-                                      (dom/set-value! (dom/by-id "token") response)
-                                      (dom/set-value! number-field (obfuscate (dom/value number-field)))
-                                      (.submit (dom/by-id "pay-form"))))}))
+  (let [c (chan)]
+    (POST "/verification" {:handler (fn [response] (go (>! c response)))})
+    (go
+      (let [response (<! c)
+            number-field (dom/by-id "number")]
+        (dom/append! (dom/by-id "pay-form") "<input id='token' type='hidden' name='token'/>")
+        (dom/set-value! (dom/by-id "token") response)
+        (dom/set-value! number-field (obfuscate (dom/value number-field)))
+        (.submit (dom/by-id "pay-form"))))))
 
 (defn submit-payment-form! []
   (do
@@ -24,6 +30,5 @@
   (when (and js/document
              (aget js/document "getElementById"))
     (ev/listen! (dom/by-id "pay-button") :click submit-payment-form!)))
-
 
 
